90b062fd9fc4c01abfa20ff84c6a45cf0924a7e1
Adarsh Kumar <adarshff06@gmail.com>
2024-07-10 22:31:43 +0530

    connect to mongodb

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async () => {
+    try {
+        const connectionObject = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MongoDB connected  ! DB HOST : ${connectionObject}`);
+        
+    } catch (error) {
+        console.log("MongoDB connection error",error);
+        process.exit(1) // READ ABOUT PROCESS FROM DOCUMENTATION OF NODE JS
+    }
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,33 @@
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+// ;( async() => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error" , (error) => {
+//             console.log("application not able to talk with database")
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT , () => {
+//             console.log(`App is listning on port ${process.env.PORT}`)
+//         })
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// })()
