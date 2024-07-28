064c591934201c667e09cc1a7ef121d1f4989af9
Adarsh Kumar <adarshff06@gmail.com>
2024-07-28 19:19:23 +0530

    write upload logic on cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"  // COME DEFAULTLY WITH NODE JS TO MANAGE THE FIL SYSTEM
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        // upload the file
+        const res = await cloudinary.uploader.upload(localFilePath , {
+            resource_type : "auto"
+        });
+
+        //file has been upload successfully
+        console.log('file upload successfully on cloudinary ', res.url);
+        return res;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // REMOVE THE LOCALLY SAVED TEMPORARY FILE AS THE UPLOAD OPERATION GET FAILED
+        return null;
+    }
+};
+
+export {uploadOnCloudinary}
