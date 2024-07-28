b04fd8a3c7d20ca515df62dbb0108a0134b15b86
Adarsh Kumar <adarshff06@gmail.com>
2024-07-28 23:30:58 +0530

    configration setup complete almost

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/temp")
+    },
+    filename: function (req, file, cb) {
+      
+      cb(null, file.originalname)  // this is not a good practice so after completing project ooptimise this file name
+      console.log("file - " , file)
+    }
+  })
+
+  export const upload = multer({ storage : storage }) // use can also write only storage here because of ES6 feature both parameter and input is eql
