6e7cf2f852cce7ba0cce79d55e9ba65a504c62b6
Adarsh Kumar <adarshff06@gmail.com>
2024-07-28 16:24:41 +0530

    use pre middleware and add password checking method

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose ,{Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     userName : {
@@ -51,5 +53,15 @@ const userSchema = new Schema({
     }
 
 } , {timestamps : true});
+
+userSchema.pre("save" , async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password , 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare("password" , this.password)
+}
 
 export const User = mongoose.model("User" , userSchema);
