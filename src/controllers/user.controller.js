bb3cdeb629dfa473cb4dc214561654baaf279476
Adarsh Kumar <adarshff06@gmail.com>
2024-08-02 23:31:11 +0530

    add logout controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -187,11 +187,39 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 const logoutUser = asyncHandler(async (req , res) => {
-    
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken : undefined
+            },
+        },
+        {
+            new : true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken" , options)
+    .clearCookie("refreshToken" , options)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "user logged out successfully"
+        )
+    )
 })
 
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.models";
+
+
+export const verifyJWT = asyncHandler(async ( _ , req , next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer" , "")
+    
+        if (!token) {
+            throw new ApiError(401 , "Unauthorised request")
+        }
+    
+        const decodedToken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if (!user) {
+            throw new ApiError(401 , "Invalid Access Token")
+        }
+    
+        req.user = user;
+        next()
+    
+    } catch (error) {
+        throw new ApiError(500 , error?.message || "Invalid Access Token")
+    }
+
+})
