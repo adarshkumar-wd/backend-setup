f7cd8cb8799bb7bfe0332437aa93c3cde65ad650
Adarsh Kumar <adarshff06@gmail.com>
2024-08-08 01:15:14 +0530

    add toggle like video controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -7,6 +7,52 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     //TODO: toggle like on video
+
+    if (!videoId) {
+        throw new ApiError(400 , "video Id not found")
+    }
+
+    const likedVideo = await Likes.findOne({Video : videoId})
+
+    if (likedVideo) {
+        
+        await Likes.deleteOne({
+            Video : videoId
+        })
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "video deslike successfully"
+            )
+        )
+        
+    } else {
+        const videoLikeData = await Likes.create({
+            Video : videoId
+        })
+
+        if (!videoLikeData) {
+            throw new ApiError(
+                500,
+                "something went wrong while creating the data"
+            )
+        }
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                videoLikeData,
+                "video like successfully"
+            )
+        )
+    }
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
