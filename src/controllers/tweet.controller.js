b5ced8f1b3652faa59af59162b056f7aeb636091
Adarsh Kumar <adarshff06@gmail.com>
2024-08-09 01:40:08 +0530

    add get user tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,47 +1,79 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweets.models.js"
-import {User} from "../models/user.models.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweets.models.js"
+import { User } from "../models/user.models.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
-    const {tweetContent} = req.body
+    const { tweetContent } = req.body
 
     if (!tweetContent) {
-        throw new ApiError(400 , "tweetContent is required")
+        throw new ApiError(400, "tweetContent is required")
     }
 
     const createTweet = await Tweet.create({
-        owner : req.user?._id,
-        content : tweetContent
+        owner: req.user?._id,
+        content: tweetContent
     })
 
     if (!createTweet) {
-        throw new ApiError(500 , "something went wrong while creating the data")
+        throw new ApiError(500, "something went wrong while creating the data")
     }
 
-    const tweetData = await Tweet.findById({_id : createTweet?._id})
+    const tweetData = await Tweet.findById({ _id: createTweet?._id })
 
     if (!tweetData) {
-        throw new ApiError(500 , "Data not fonud in database")
+        throw new ApiError(500, "Data not fonud in database")
     }
 
     return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            tweetData,
-            "tweet create successfully"
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                tweetData,
+                "tweet create successfully"
+            )
         )
-    )
 
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
+    const { userId } = req.params
     // TODO: get user tweets
+
+    if (!userId || !isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user id found")
+    }
+
+    const checkUserTweet = await Tweet.findOne({ owner: userId })
+
+    console.log(checkUserTweet)
+
+    if (!checkUserTweet) {
+        return res
+            .status(404)
+            .json(
+                new ApiResponse(
+                    404,
+                    checkUserTweet,
+                    "user does not tweet anything"
+                )
+            )
+    } else {
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    checkUserTweet,
+                    "user tweet fetched successfully"
+                )
+            )
+    }
+
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
