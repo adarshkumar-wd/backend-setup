f64f8bccf17996d9f23e5a34d5744d8675dee40d
Adarsh Kumar <adarshff06@gmail.com>
2024-08-09 00:57:37 +0530

    add create tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,6 +7,37 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {tweetContent} = req.body
+
+    if (!tweetContent) {
+        throw new ApiError(400 , "tweetContent is required")
+    }
+
+    const createTweet = await Tweet.create({
+        owner : req.user?._id,
+        content : tweetContent
+    })
+
+    if (!createTweet) {
+        throw new ApiError(500 , "something went wrong while creating the data")
+    }
+
+    const tweetData = await Tweet.findById({_id : createTweet?._id})
+
+    if (!tweetData) {
+        throw new ApiError(500 , "Data not fonud in database")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            tweetData,
+            "tweet create successfully"
+        )
+    )
+
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
